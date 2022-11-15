ef0b8e204f2ebc9bafabcbc265c585158be98a48
ffdietz <felipefierrod@gmail.com>
2022-11-15 15:41:01 +0100

    header login button

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,29 +1,46 @@
-import { Flex, Text } from "@chakra-ui/react"
-import { Link } from "react-router-dom"
+import { Box, Flex, Heading, Text } from "@chakra-ui/react"
+import { Link, useNavigate } from "react-router-dom"
+import { AUTH_TOKEN } from "../helpers/constants";
 
 function Header() {
+  const navigate = useNavigate();
+  const authToken = localStorage.getItem(AUTH_TOKEN);
   return (
     <Flex
       position="fixed"
       bg="orange.500"
       w="full"
-      p="1rem"
+      p="1.2rem"
       alignItems="center"
+      fontSize="x-large"
     >
       <Link to="/">
-        <Text
-          borderRight="2px"
-          p="0 20px"
-          fontWeight="extrabold"
-          fontSize="4xl"
-        >
-          Hacker News
-        </Text>
+        <Heading borderRight="2px" p="0 20px">Hacker News</Heading>
       </Link>
       <Flex justifyContent="space-between" w="15vw" marginLeft="3rem">
         <Link to="/">new</Link>
-        <Link to="/create">submit</Link>
-        <Link to="/login">login</Link>
+        <Link to="/search">search</Link>
+        {authToken && (
+          <Link to="/create">submit</Link>
+        )}
+      </Flex>
+      <Flex w="full" justifyContent="flex-end" paddingRight="2rem">
+        {authToken?(
+          <Box
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              navigate("/");
+            }}
+          >
+            logout
+          </Box>
+        ) : (
+          <Link
+            to="login"
+          >
+            login
+          </Link>
+        )}
       </Flex>
     </Flex>
   );
